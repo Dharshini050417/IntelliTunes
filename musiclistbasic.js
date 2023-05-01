02659fda7ecbcf338e5584dbe6c807852358f269
Dharshini050417 <115538571+Dharshini050417@users.noreply.github.com>
2023-05-01 19:42:47 +0530

    Add files via upload

---
+++ musiclistbasic.js
@@ -0,0 +1,19 @@
+let allMusic = [
+
+    {
+        name: "Yamunai Atrile",
+        src: "1"
+    },
+    {
+        name: "Kattu sirukki",
+        src:"2"
+    },
+    {
+        name: "Manmadha rasa",
+        src:"3"
+    },
+    {
+        name: "Loose Penne",
+        src:"4"
+    },
+];
